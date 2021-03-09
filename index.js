67709143a4bab13cd7ad38b012b671a0bdbc4ca7
Jeremy <lavigne.jeremy@post.com>
2021-03-09 20:21:59 +0100

    Clean localisations in json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // const getPokemonList = require("./src/getPokemonList");
-// const fs = require('fs');
+const fs = require('fs');
 
 // getPokemonList().then(list => {
 //     const toWrite = JSON.stringify(list, null, 2);
@@ -9,3 +9,56 @@
 //         console.log('Saved!');
 //     });
 // })
+
+// const pokedex = require('./assets/pokedex.json');
+
+// for (let i = 0; i < pokedex.length; i += 1) {
+//     const current = pokedex[i];
+//     const loca = current.localisation
+
+//     for (let j = 0; j < loca.length; j += 1) {
+//         // Remove spaces
+//         loca[j] = loca[j].trim()
+
+//         // Check last one
+//         if (j === loca.length - 1) {
+//             const indexEvo = loca[j].search('Évolution');
+//             if (indexEvo !== -1) {
+//                 current.evo = loca[j].substr(indexEvo)
+//                 loca[j] = loca[j].substr(0, indexEvo);
+//             }
+//         }
+//     }
+// }
+
+// const toWrite = JSON.stringify(pokedex, null, 2);
+
+// fs.writeFile('./assets/pokedex2.json', toWrite, function (err) {
+//     if (err) throw err;
+//     console.log('Saved!');
+// });
+
+// const pokedex = require('./assets/pokedex2.json');
+
+// for (let i = 0; i < pokedex.length; i += 1) {
+//     const current = pokedex[i];
+//     const loca = current.localisation
+
+//     for (let j = 0; j < loca.length; j += 1) {
+
+//         // Check last one
+//         if (loca.length === 1) {
+//             if (loca[j] === "") {
+//                 loca.pop();
+//             }
+//         }
+//     }
+// }
+
+// console.log(pokedex[1]);
+// const toWrite = JSON.stringify(pokedex, null, 2);
+
+// fs.writeFile('./assets/pokedex.json', toWrite, function (err) {
+//     if (err) throw err;
+//     console.log('Saved!');
+// });
