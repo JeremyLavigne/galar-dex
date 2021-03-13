51de9e13976cc7646098c535cf0126aa6588dab2
Jeremy <lavigne.jeremy@post.com>
2021-03-13 19:27:26 +0100

    Image full size in modal

--- src/components/MoreDetails.js
+++ src/components/MoreDetails.js
@@ -1,20 +1,44 @@
-function Line({ pokemon, setModalIsOpen, setActivePokemon }) {
+function MoreDetails({ pokemon, setModalIsOpen, setActivePokemon }) {
 
     const handleClose = () => {
         setModalIsOpen(false);
         setActivePokemon({});
     }
 
+    const image = `https://pokeres.bastionbot.org/images/pokemon/${pokemon.nationalId}.png`
+
     return (
         <>
             <div className="around-modal" onClick={handleClose}></div>
             <div className="modal">
-                {pokemon.evo}
-                {pokemon.localisation}
-                <button onClick={handleClose}>Close</button>
+                <div className="modal-content">
+                    <div className="modal-content-text">
+                        { pokemon.evo !== "" ?
+                            <div>
+                                <h3>Evolution</h3>
+                                {pokemon.evo}
+                            </div>
+                            :
+                            null
+                        }
+                        { pokemon.localisation.length > 0 ?
+                            <div>
+                                <h3>Localisation</h3>
+                                {
+                                    pokemon.localisation.map((loc) => <li key={loc}>{loc}</li>)
+                                }
+                            </div>
+                            :
+                            null
+                        }
+                    </div>
+                    <img class="image-full-size" alt="artwork" src={image} />
+                </div>
+
+                <button className="modal-close" onClick={handleClose}>Close</button>
             </div>
         </>
     );
 }
 
-export default Line;
+export default MoreDetails;
